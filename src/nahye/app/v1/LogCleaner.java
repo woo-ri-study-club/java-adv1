a2779f9d542dc519912518a1fc0dc5cf2ac8d020
nahye <nnhh.shine@gmail.com>
2025-02-19 02:01:35 +0900

    LogCleaner 수정!

--- src/nahye/app/v1/LogCleaner.java
+++ src/nahye/app/v1/LogCleaner.java
@@ -1,5 +1,11 @@
 package nahye.app.v1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 
 public class LogCleaner implements Runnable {
@@ -15,12 +21,8 @@ public class LogCleaner implements Runnable {
 
         try {
             while (true) {
-                if (queue.size() >= 10) {
-                    int itemsToRemove = queue.size() - 10;
-                    for (int i = 0; i < itemsToRemove; i++) {
-                        queue.poll();
-                    }
-                }
+                cleanMemoryQueue();
+                cleanLogFile();
                 Thread.sleep(5000);
             }
         } catch (InterruptedException e) {
@@ -28,4 +30,30 @@ public class LogCleaner implements Runnable {
         }
     }
 
+    private void cleanMemoryQueue() {
+        if (queue.size() > 10) {
+            int itemsToRemove = queue.size() - 10;
+            for (int i = 0; i < itemsToRemove; i++) {
+                queue.poll();
+            }
+        }
+    }
+
+    private void cleanLogFile() {
+
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get("news_log.txt"));
+
+            if (allLines.size() > 10) {
+                List<String> newLines = new ArrayList<>();
+                for (int i = allLines.size() - 10; i < allLines.size(); i++) {
+                    newLines.add(allLines.get(i));
+                }
+                Files.write(Paths.get("news_log.txt"), newLines, StandardOpenOption.TRUNCATE_EXISTING);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
