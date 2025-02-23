2a30826be4445b29f7ce5ee6666d59bba60cf1c4
chaewonJo <ch730268@gmail.com>
2025-02-23 22:55:50 +0900

    refactor. LogCleaner 미션 1과 동기화 및 리뷰 반영

--- chaewon/app/v2/util/LogCleaner.java
+++ chaewon/app/v2/util/LogCleaner.java
@@ -6,23 +6,23 @@ public class LogCleaner implements Runnable {
 
     @Override
     public void run() {
-        while (!Thread.interrupted()) {
+        while (!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(5000);
 
                 while (getLogQueue().size() > getLogSize()) {
                     String oldLog = removeOldLog();
 
-                    if(oldLog != null){
-                        String deleteLog = logFormatter("DELETE",oldLog);
+                    if (oldLog != null) {
+                        String deleteLog = logFormatter("DELETE", oldLog);
 
                         System.out.println(deleteLog);
-                        logFileWriter(deleteLog);
                     }
                 }
 
             } catch (InterruptedException e) {
                 errorLog("클리너 실패: " + e.getMessage());
+                Thread.currentThread().interrupt();
             }
         }
     }
